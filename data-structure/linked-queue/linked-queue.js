7f57545071c5ba39f9ed06cbe82f0f519e0fdd89
yazalde filimone <yazaldefilimon@gmail.com>
2022-08-03 16:06:10 +0200

    feat: make initial setup of linked queue

---
+++ data-structure/linked-queue/linked-queue.js
@@ -0,0 +1,44 @@
+function LinkedQueue() {
+  var Node = function (element) {
+    this.element = element;
+    this.next = null;
+  };
+
+  var length = 0;
+  var head = null;
+
+  this.append = function (element) {
+    // add a element in end queue
+  };
+
+  this.insert = function (position, element) {
+    // add a  element in a specify position
+  };
+
+  this.removeAt = function (position) {
+    // remove element using position
+  };
+
+  this.remove = function (element) {
+    // remove element will go in param
+  };
+
+  this.indexOf = function (element) {
+    // return position of element
+  };
+
+  this.isEmpty = function () {
+    // return is empty or not empty the instance
+  };
+
+  this.size = function () {
+    // return the size of instance
+  };
+
+  this.toString = function () {
+    // converte in string
+  };
+  this.print = function () {
+    // show in console
+  };
+}
