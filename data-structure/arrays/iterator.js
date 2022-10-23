100a05f68c80b6523c8d0ba943ed6f402ce33ce9
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-10-23 19:54:29 +0200

    feat: data structure @@iterator

---
+++ data-structure/arrays/iterator.js
@@ -0,0 +1,11 @@
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+let iterator = numbers[Symbol.iterator]();
+console.log(iterator.next().value); //1
+console.log(iterator.next().value); //2
+console.log(iterator.next().value); //3
+iterator = numbers[Symbol.iterator]();
+
+for (const n of iterator) {
+  // console.log(n);
+}
