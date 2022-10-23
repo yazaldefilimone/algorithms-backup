6334dc524fbedceeac8f7e021069e7aa7d357f62
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-10-23 20:14:15 +0200

    data structure: keys

---
+++ data-structure/arrays/keys.js
@@ -0,0 +1,10 @@
+let numbers = [1, 2, 3, 4, 5];
+
+const aKeys = numbers.keys();
+
+console.log(aKeys.next());
+console.log(aKeys.next());
+console.log(aKeys.next());
+console.log(aKeys.next());
+console.log(aKeys.next());
+console.log(aKeys.next());
