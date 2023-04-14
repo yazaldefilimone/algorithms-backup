cd0b5df42fd928681489419214e22bfcfbef79cd
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-04-14 09:53:46 +0200

    *

---
+++ coding_interviews/leetcode/easy/two-sum.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  for (let index = 0; index < nums.length; index++) {
+    const t = nums[index];
+    nums[index] = null;
+    const i = nums.indexOf(target - t);
+    if (i !== -1 && i !== index) return [index, i];
+  }
+};
