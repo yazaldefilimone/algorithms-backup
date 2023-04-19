429b650d997377377783ebe5b634c1caf55cd567
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-04-19 08:38:42 +0200

    *

---
+++ competitive_programming/basic-concepts/javascript/data_type.js
@@ -0,0 +1,19 @@
+const num = 29;
+const str = '121';
+const bool = true;
+const obj = { num };
+let undefined_variable;
+// const undefined_const;
+// console.log(typeof num);
+// console.log(typeof str);
+// console.log(typeof bool);
+// console.log(typeof num);
+// console.log(typeof obj);
+// console.log(typeof undefined_variable);
+
+// console.log(obj);
+// obj.num = 30;
+// console.log(obj);
+
+// console.log(str.indexOf('hd'));
+// console.log(str.lastIndexOf('1', 1));
