b7f83199de0e0fbe0ce8bad4138e652a61e8273f
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-04-20 10:20:57 +0200

    *

---
+++ coding_interviews/leetcode/easy/longest-common-prefix.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+function longestCommonPrefix(strs) {
+  if (strs.length === 0) return "";
+  let prefix = strs[0];
+  for (let i = 1; i < strs.length; i++) {
+    while (strs[i].indexOf(prefix) !== 0) {
+      prefix = prefix.substring(0, prefix.length - 1);
+      if (prefix === "") return "";
+    }
+  }
+  return prefix;
+}
