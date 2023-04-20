7dd61da0193ba84476bf2c535b7876396bebe8aa
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-04-20 10:27:41 +0200

    *

--- computer_science/algorithms/binary_search/index.rs
+++ computer_science/algorithms/binary_search/index.rs
@@ -1,11 +1,3 @@
-fn main() {
-    let arr: [i32; 18] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 3, 14, 1, 15, 16];
-    let result = binary_search(&arr, 3);
-    match result {
-        Some(i) => println!("O elemento 3 foi encontrado na posição {}", i),
-        None => println!("O elemento 3 não foi encontrado no array"),
-    }
-}
 fn binary_search(elements: &[i32], search_data: i32) -> Option<usize> {
     let mut down = 0;
     let mut up = elements.len() - 1;
