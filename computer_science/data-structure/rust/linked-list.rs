5f444f11307fc593ffb839c26e817ef2d833e481
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-04-22 21:50:05 +0200

    *

---
+++ computer_science/data-structure/rust/linked-list.rs
@@ -0,0 +1,66 @@
+class Node {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  isEmpty() {
+    return this.head === null;
+  }
+
+  pushFront(value) {
+    this.head = new Node(value, this.head);
+  }
+
+  pushBack(value) {
+    let currentNode = this.head;
+
+    while (currentNode && currentNode.next) {
+      currentNode = currentNode.next;
+    }
+    currentNode.next = new Node(value);
+  }
+
+  size() {
+    let currentNode = this.head;
+    let count = 0;
+    while (currentNode) {
+      count += 1;
+      currentNode = currentNode.next;
+    }
+    return count;
+  }
+
+  search(value) {
+    let currentNode = this.head;
+    while (currentNode && currentNode.value === value) {
+      currentNode = currentNode.next;
+    }
+    return Boolean(currentNode);
+  }
+  remove(value) {
+    if (this.isEmpty()) {
+      return;
+    }
+
+    if (this.head.value === value) {
+      this.head = this.head.next;
+      return;
+    }
+
+    let currentNode = this.head;
+    while (currentNode.next) {
+      if (currentNode.next.value === value) {
+        currentNode.next = currentNode.next.next;
+        return;
+      }
+      currentNode = currentNode.next;
+    }
+  }
+}
