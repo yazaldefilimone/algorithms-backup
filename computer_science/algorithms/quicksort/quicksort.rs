340ed72504045de0bfbc4bae01cf8581c3604f02
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-04-25 11:16:36 +0200

    *

---
+++ computer_science/algorithms/quicksort/quicksort.rs
@@ -0,0 +1,20 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+fn quicksort(elements: &vec!<i64>) -> &'static [i64] {
+    if elements.len() < 2 {
+        return elements;
+    } else {
+  
+       
+    }
+}
+
+fn isBigger(e: i64, quite:i64) -> bool{
+    return  e > quite
+}
+
+fn isSmaller(e: i64, quite:i64)-> bool{
+    return  e < quite
+}
