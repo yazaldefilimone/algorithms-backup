fde0f11a78af158f1695081ac24d96e3e0f5bc08
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-05-03 21:33:26 +0200

    *

--- computer_science/data-structure/tree/binary-tree.js
+++ computer_science/data-structure/tree/binary-tree.js
@@ -1,3 +1,4 @@
+import { Queue } from "../queue/queue.js";
 class BinaryTree {
   constructor(value) {
     this.value = value;
@@ -22,6 +23,35 @@ class BinaryTree {
       this.right = node;
     } else {
       this.right = node;
+    }
+  }
+
+  preOrder() {
+    console.log(this.value);
+    if (this.left) this.left.preOrder();
+    if (this.right) this.right.preOrder();
+  }
+
+  inOrder() {
+    if (this.left) this.left.inOrder();
+    console.log(this.value);
+    if (this.right) this.right.inOrder();
+  }
+
+  postOrder() {
+    if (this.left) this.left.postOrder();
+    if (this.right) this.right.postOrder();
+    console.log(this.value);
+  }
+
+  bfs() {
+    const queue = new Queue();
+    queue.enqueue(this);
+    while (!queue.isEmpty()) {
+      const node = queue.dequeue();
+      console.log(node.value);
+      if (node.left) queue.enqueue(node.left);
+      if (node.right) queue.enqueue(node.right);
     }
   }
 }
