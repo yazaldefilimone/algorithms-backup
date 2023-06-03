e262f553ac2731910f0dc22f3730956c7b350269
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-06-03 20:38:31 +0200

    *

--- computer_science/data-structure/doubly-linked-list/doubly-linked-list.test.js
+++ computer_science/data-structure/doubly-linked-list/doubly-linked-list.test.js
@@ -9,26 +9,26 @@ describe("Doubly Linked List", () => {
     expect(doublyLinkedList.head.prev).toBe(null);
     expect(doublyLinkedList.tail.value).toBe(1);
   });
-
-  it("method push", () => {
-    const doublyLinkedList = new DoublyLinkedList(1);
-
-    doublyLinkedList.push(2);
-    expect(doublyLinkedList.head.value).toBe(1);
-    expect(doublyLinkedList.head.next.value).toBe(2);
-    expect(doublyLinkedList.head.prev).toBe(null);
-    expect(doublyLinkedList.tail.value).toBe(2);
-  });
-
-  it("method pop", () => {
-    const doublyLinkedList = new DoublyLinkedList(1);
-    doublyLinkedList.push(2);
-    doublyLinkedList.push(3);
-    doublyLinkedList.pop();
-    expect(doublyLinkedList.head.value).toBe(1);
-    expect(doublyLinkedList.head.next.value).toBe(2);
-    expect(doublyLinkedList.tail.prev.value).toBe(1);
-    expect(doublyLinkedList.tail.next).toBe(null);
-    expect(doublyLinkedList.tail.value).toBe(2);
-  });
+  // it("method pushFront", () => {
+  //   const doublyLinkedList = new DoublyLinkedList(1);
+  //   doublyLinkedList.pushFront(2);
+  //   expect(doublyLinkedList.head.value).toBe(2);
+  //   expect(doublyLinkedList.head.next.value).toBe(1);
+  // });
+  // it("method pop", () => {
+  //   const doublyLinkedList = new DoublyLinkedList(1);
+  //   doublyLinkedList.push(2);
+  //   doublyLinkedList.push(3);
+  //   doublyLinkedList.pop();
+  //   expect(doublyLinkedList.tail.value).toBe(2);
+  // });
+  // it("method pushBack", () => {
+  //   const doublyLinkedList = new DoublyLinkedList(1);
+  //   doublyLinkedList.pushFront(2);
+  //   doublyLinkedList.pushFront(3);
+  //   doublyLinkedList.pushBack(5);
+  //   expect(doublyLinkedList.head.value).toBe(3);
+  //   expect(doublyLinkedList.tail.value).toBe(5);
+  //   expect(doublyLinkedList.tail.prev.value).toBe(3);
+  // });
 });
