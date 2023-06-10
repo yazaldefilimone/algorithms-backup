f37434957b076fec8881a23c185ebe5ca4316e0f
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-06-10 21:03:35 +0200

    *

--- computer_science/data-structure/queue/queue.js
+++ computer_science/data-structure/queue/queue.js
@@ -1,28 +1,38 @@
+export class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
 export class Queue {
   constructor() {
-    this.elements = [];
+    this.first = null;
+    this.last = null;
+    this.length = 0;
   }
-  enqueue(element) {
-    this.elements.push(element);
+  enqueue(value) {
+    const node = new Node(value);
+    if (!this.last && this.length == 0) {
+      this.first = node;
+      this.last = node;
+    } else {
+      this.last.next = node;
+      this.last = node;
+    }
+    this.length++;
+    return this;
   }
 
   dequeue() {
-    return this.elements.shift();
-  }
-
-  front() {
-    return this.elements[0];
-  }
-
-  back() {
-    return this.elements.at(-1);
-  }
-
-  size() {
-    return this.elements.length;
-  }
-
-  isEmpty() {
-    return this.elements.length === 0;
+    if (!this.first) return undefined;
+    const temp = this.first;
+    this.first = temp.next;
+    if (this.first === null) {
+      this.last = null;
+    }
+    temp.next = null;
+    this.length--;
+    return temp;
   }
 }
