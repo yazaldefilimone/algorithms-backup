9559666306fe697c1d9eabca72b563d31803f176
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-06-16 17:59:31 +0200

    *

--- coding_interviews/leetcode/easy/dynamic-programming/tests/climbing-stairs.test.js
+++ coding_interviews/leetcode/easy/dynamic-programming/tests/climbing-stairs.test.js
@@ -1,7 +1,10 @@
 import { it, expect, describe } from "vitest";
-import { climbStairs } from "coding_interviews/leetcode/easy/dynamic-programming/climbing-stairs";
+import {
+  climbStairs_with_pointer,
+  climbStairs,
+} from "coding_interviews/leetcode/easy/dynamic-programming/climbing-stairs";
 
-describe("Valid Anagram", () => {
+describe("Climb Stairs", () => {
   it("n=2 ------> 2", () => {
     expect(climbStairs(2)).toBe(2);
   });
@@ -13,5 +16,20 @@ describe("Valid Anagram", () => {
   });
   it("n=7 ------> 21", () => {
     expect(climbStairs(7)).toBe(21);
+  });
+});
+
+describe("Climb Stairs Second", () => {
+  it("n=2 ------> 2", () => {
+    expect(climbStairs_with_pointer(2)).toBe(2);
+  });
+  it("n=3 ------> 3", () => {
+    expect(climbStairs_with_pointer(3)).toBe(3);
+  });
+  it("n=5 ------> 8", () => {
+    expect(climbStairs_with_pointer(5)).toBe(8);
+  });
+  it("n=7 ------> 21", () => {
+    expect(climbStairs_with_pointer(7)).toBe(21);
   });
 });
