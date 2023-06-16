c074df6893698ae811aaa8a83c6abce2fc7ef0a8
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-06-16 17:51:40 +0200

    *

---
+++ coding_interviews/leetcode/easy/dynamic-programming/climbing-stairs.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+
+// time: O(n)
+// space:  O(n)
+export function climbStairs(n) {
+  if (n <= 3) return n;
+  let ways = [0, 1, 2, 3];
+
+  for (let index = 4; index <= n; index++) {
+    ways.push(ways[index - 1] + ways[index - 2]);
+  }
+  return ways.pop();
+}
