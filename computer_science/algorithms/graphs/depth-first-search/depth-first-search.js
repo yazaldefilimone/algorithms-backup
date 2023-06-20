ce724ad29449e7e74e0608da84fe364fb8dca4de
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-06-20 18:02:04 +0200

    *

--- computer_science/algorithms/graphs/depth-first-search/depth-first-search.js
+++ computer_science/algorithms/graphs/depth-first-search/depth-first-search.js
@@ -1,10 +1,12 @@
-export function depthFirstSearch(root) {
-  let results = [];
-  function traverse(currentNode) {
-    results.push(currentNode?.value);
-    if (currentNode.left) traverse(currentNode.left);
-    if (currentNode.right) traverse(currentNode.right);
-  }
-  traverse(root);
-  return results;
-}
+export const depthFirstSearch = {
+  preOrder(root) {
+    let results = [];
+    function traverse(currentNode) {
+      results.push(currentNode?.value);
+      if (currentNode.left) traverse(currentNode.left);
+      if (currentNode.right) traverse(currentNode.right);
+    }
+    traverse(root);
+    return results;
+  },
+};
