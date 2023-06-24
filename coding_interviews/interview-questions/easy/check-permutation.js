458888caa3ae313b5200e38940bfdeaf5ddfd830
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-06-24 10:39:28 +0200

    *

---
+++ coding_interviews/interview-questions/easy/check-permutation.js
@@ -0,0 +1,16 @@
+export const checkPermutation = (setence1, setence2) => {
+  if (setence1.length !== setence2.length) {
+    return false;
+  }
+  let hash = {};
+  for (let index = 0; index < setence1.length; index++) {
+    hash[setence1[index]] = true;
+  }
+
+  for (let index = 0; index < setence2.length; index++) {
+    if (!hash[setence2[index]]) {
+      return false;
+    }
+  }
+  return true;
+};
