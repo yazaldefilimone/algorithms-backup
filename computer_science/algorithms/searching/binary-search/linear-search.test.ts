ca781b5f208639d562c827d17ba1e1fdc4c7af77
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-10-17 20:14:35 +0200

    *

--- computer_science/algorithms/searching/binary-search/linear-search.test.ts
+++ computer_science/algorithms/searching/binary-search/linear-search.test.ts
@@ -1,15 +1,15 @@
 import { it, expect, describe } from "vitest";
-import { linear_search_array } from "../linear-search";
+import { binary_search } from "./binary-search";
 
 describe("computer-science/algorithms/searching/array/linear-search", () => {
   it("case #1, should true", () => {
     const hey_stack = [1, 2, 3, 4, 5];
     const target = 4;
-    expect(linear_search_array(hey_stack, target)).toBe(true);
+    expect(binary_search(hey_stack, target)).toBe(true);
   });
   it("case #2, should false", () => {
     const hey_stack = [1, 2, 3, 4, 5];
     const target = 0;
-    expect(linear_search_array(hey_stack, target)).toBe(false);
+    expect(binary_search(hey_stack, target)).toBe(false);
   });
 });
