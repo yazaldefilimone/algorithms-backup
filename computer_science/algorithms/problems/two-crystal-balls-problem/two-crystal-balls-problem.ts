a228a26a6fb0e4a2cfe7de1127d63788d5d34a77
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-10-17 21:42:50 +0200

    *

---
+++ computer_science/algorithms/problems/two-crystal-balls-problem/two-crystal-balls-problem.ts
@@ -0,0 +1,21 @@
+export function two_crystal_balls(breaks: boolean[]): number {
+  const amount = Math.floor(Math.sqrt(breaks.length));
+
+  let index = amount;
+
+  for (; index < breaks.length; index += amount) {
+    if (breaks[index]) {
+      break;
+    }
+  }
+
+  index -= amount;
+
+  for (let idx = 0; idx < amount && index < breaks.length; idx++, index++) {
+    if (breaks[index]) {
+      return index;
+    }
+  }
+
+  return -1;
+}
