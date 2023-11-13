435b9ff390f0d61ffc27e8f68e84a58444bed4d9
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-11-13 19:17:46 +0200

    *

--- coding_interviews/leetcode/intersection-of-two-linked-lists/intersection-of-two-linked-lists.js
+++ coding_interviews/leetcode/intersection-of-two-linked-lists/intersection-of-two-linked-lists.js
@@ -11,51 +11,20 @@
  * @param {ListNode} headB
  * @return {ListNode}
  */
-//  runtime:70ms
-function getIntersectionNode(headA, headB) {
-  if (headA === headB) return headB;
-  if (headA.next === headB) return headB;
-  if (headB.next === headA) return headA;
+function getIntersectionNodePref(headA, headB) {
   let _headA = headA;
-  const table = new Map();
-  while (_headA !== null) {
-    table.set(_headA, true);
-    if (_headA.next) table.set(_headA.next, true);
-    if (_headA.next?.next) table.set(_headA.next?.next, true);
-    if (_headA.next?.next?.next) table.set(_headA.next?.next?.next);
-    _headA = _headA.next?.next?.next || null;
-  }
   let _headB = headB;
-  while (_headB !== null) {
-    if (table.get(_headB)) return _headB;
-    if (_headB.next && table.get(_headB.next)) return _headB.next;
-    if (_headB.next?.next && table.get(_headB.next?.next)) return _headB.next?.next;
-    if (_headB.next?.next?.next && table.get(_headB.next?.next?.next)) return _headB.next?.next?.next;
-    _headB = _headB.next?.next?.next || null;
-  }
-  return null;
-}
-// 1 - 2 - 3 - 4 - 5 - 6
-// 0 - 9 - 8 - 4 - 5 - 6
-// runtime: 86ms
-function getIntersectionNodeFirst(headA, headB) {
-  if (headA === headB) return headB;
-  if (headA.next === headB) return headB;
-  if (headB.next === headA) return headA;
-  let _headA = headA;
-  const table = new Map();
-  while (_headA !== null) {
-    table.set(_headA, _headA);
-    _headA = _headA.next;
-  }
-  let _headB = headB;
-  while (_headB !== null) {
-    const last = table.get(_headB);
-    if (last) {
-      return _headB;
+  while (_headA !== _headB) {
+    if (_headA == null) {
+      _headA = headB;
+    } else {
+      _headA = _headA.next;
     }
-    _headB = _headB.next;
+    if (_headB == null) {
+      _headB = headA;
+    } else {
+      _headB = _headB.next;
+    }
   }
-
-  return null;
+  return _headA;
 }
