b1a796d63bcc54e2b16da9ec14e7aba1d5723de5
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-12-01 04:11:08 +0200

    *

--- coding_interviews/leetcode/sqrt-x/sqrt-x.js
+++ coding_interviews/leetcode/sqrt-x/sqrt-x.js
@@ -2,7 +2,10 @@
  * @param {number} x
  * @return {number}
  */
-var mySqrt = function (x) {
+
+//  Time: O(log n) || Space: O(1) || 61ms runtime || 42.67 MB
+function square_root_binary_search(x) {
+  if (x === 1 || x === 0 || x === 2) return 1;
   let right = x;
   let left = 0;
   while (left <= right) {
@@ -19,4 +22,23 @@ var mySqrt = function (x) {
     return middle;
   }
   return right;
-};
+}
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
+//  Time: O(log n) || Space: O(1) || 50ms runtime || 43.96 MB
+function square_root_binary_search_newton(x) {
+  if (x === 0 || x === 1) {
+    return x;
+  }
+  let guess = x / 2;
+  while (true) {
+    const newGuess = (guess + x / guess) / 2;
+    if (Math.abs(newGuess - guess) < 1) {
+      return Math.floor(newGuess);
+    }
+    guess = newGuess;
+  }
+}
