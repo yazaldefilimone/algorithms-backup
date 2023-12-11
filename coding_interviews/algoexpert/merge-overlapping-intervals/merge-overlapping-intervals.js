5e069cd1ea91212b12815e49839f9cb437c295bd
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-12-11 13:03:47 +0200

    *

--- coding_interviews/algoexpert/merge-overlapping-intervals/merge-overlapping-intervals.js
+++ coding_interviews/algoexpert/merge-overlapping-intervals/merge-overlapping-intervals.js
@@ -1,17 +1,46 @@
-function mergeOverlappingIntervals(array) {
-  const overlapping = [];
-  for (let index = 0; index < array.length; index++) {
-    const over = array[index];
-    for (let nextId = index; nextId < array.length; nextId++) {
-      const next = array[nextId];
-      if (over[0] > next[0] || over[1] > next[0]) {
-        over[1] = next[1];
-      } else {
-        // overlapping.push(over);
-        // overlapping.push(next);
-      }
+/**
+ *
+ * @param {number[][]} array
+ * @returns {number[][]}
+ */
+function mergeOverlappingIntervals(intervals) {
+  const sortedIntervals = intervals.sort((left, right) => left[0] - right[0]);
+  let currentInterval = sortedIntervals[0];
+  const mergeOverlapping = [currentInterval];
+  for (const nextInterval of sortedIntervals) {
+    const currentIntervalEnd = currentInterval[1];
+    const [nextIntervalStart, nextIntervalEnd] = nextInterval;
+
+    if (currentIntervalEnd >= nextIntervalStart) {
+      currentInterval[1] = Math.max(currentIntervalEnd, nextIntervalEnd);
+    } else {
+      currentInterval = nextInterval;
+      mergeOverlapping.push(currentInterval);
     }
-    overlapping.push(over);
   }
-  return overlapping;
+
+  return mergeOverlapping;
 }
+/*
+[
+  [1, 2],
+  [3, 8],
+  [9, 10]
+]
+[
+  [-50, 20],
+  [70, 95]
+]
+ */
+console.log(
+  mergeOverlappingIntervals([
+    [89, 90],
+    [-10, 20],
+    [-50, 0],
+    [70, 90],
+    [90, 91],
+    [90, 95],
+  ])
+);
+// Do not edit the line below.
+// exports.mergeOverlappingIntervals = mergeOverlappingIntervals;
