bc5657f8a21c418fc43e7eadacc7827190dd2b9b
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-12-31 19:45:14 +0200

    *

---
+++ computer_science/data-structure/hash-table/check-voter.py
@@ -0,0 +1,10 @@
+voted = {}
+
+
+def checkVoter(name):
+    isVoted = voted.get(name)
+    if isVoted:
+        print("You already voted, you can't vote again")
+    else:
+        voted[name] = True
+        print("Voted!")
