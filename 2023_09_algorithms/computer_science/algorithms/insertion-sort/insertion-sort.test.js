5b3a453e3fa3b5cc699d2471f449b5904398dd0e
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-12-31 19:45:14 +0200

    *

--- 2023_09_algorithms/computer_science/algorithms/insertion-sort/insertion-sort.test.js
+++
@@ -1,9 +0,0 @@
-import { it, expect, describe } from "vitest";
-import { insertionSort } from "./insertion-sort";
-
-describe("Insertion Sort", () => {
-  it("[6,3,4,2,5,1] ---> [1,2,3,4,5,6]", () => {
-    const sorted = insertionSort([6, 3, 4, 2, 5, 1]);
-    expect(sorted).toEqual([1, 2, 3, 4, 5, 6]);
-  });
-});
