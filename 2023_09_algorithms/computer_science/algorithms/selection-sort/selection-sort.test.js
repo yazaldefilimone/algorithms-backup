b5bb23016a792067288c71622397fcd938274e8e
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-12-31 19:45:14 +0200

    *

--- 2023_09_algorithms/computer_science/algorithms/selection-sort/selection-sort.test.js
+++
@@ -1,9 +0,0 @@
-import { it, expect, describe } from "vitest";
-import { selectionSort } from "./selection-sort";
-
-describe("Selection Sort", () => {
-  it("[6,3,4,2,5,1] ---> [1,2,3,4,5,6]", () => {
-    const sorted = selectionSort([6, 3, 4, 2, 5, 1]);
-    expect(sorted).toEqual([1, 2, 3, 4, 5, 6]);
-  });
-});
